caaed64dcda3e72e2d51800095b2cbcc8ea6ae9b
root <root@Fragua.home>
2017-06-26 17:47:31 -0400

    Estructura en Carpetas

---
+++ paginaWeb/Home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Seguros Chile Regional</title>
+  <link href="css/estilos.css" rel="stylesheet"type="text/css"/>
+</head>
+<body>
+  <!-- Navegador -->
+  <?php include 'Navegador.php' ?>
+  <!-- publicidad -->
+  <div class="publicidad">
+    <img src="imagenes/planjoven.jpg"/>
+  </div>
+  <!-- Contenido -->
+  <!-- referencia de usuarios -->
+  <!-- footer -->
+</body>
+</html>
---
+++ paginaWeb/Navegador.php
@@ -0,0 +1,19 @@
+<?php
+echo'<nav class="navegador">
+  <div class="logo">
+  </div>
+  <div class="portal">
+    <button class="ingresar">ingresar</button>
+    <a href="#">ingresar a otro potal</a>
+  </div>
+  <div class="menu">
+    <ul>
+      <li><a href="#">Chile Regional</a></li>
+      <li><a href="#">Seguros</a></li>
+      <li><a href="#">Consejos y servicios</a></li>
+      <li><a href="#">Contacto</a></li>
+    </ul>
+  </div>
+</nav>
+';
+ ?>
